6ce7fd27f9d04b7c8cbbf9ae40c3d5f39b7f536e
SooHyun Park <vivian0304@naver.com>
2023-07-07 18:38:37 +0900

    init: 초기세팅

---
+++ pocketpt/src/main/java/com/madeyepeople/pocketpt/PocketptApplication.java
@@ -0,0 +1,13 @@
+package com.madeyepeople.pocketpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketptApplication.class, args);
+	}
+
+}
---
+++ pocketpt/src/test/java/com/madeyepeople/pocketpt/PocketptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madeyepeople.pocketpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
