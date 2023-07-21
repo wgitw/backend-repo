111b475b069754de291928a5b5c4aec6babbe8f3
san <55998816+kimtks456@users.noreply.github.com>
2023-07-22 02:52:08 +0900

    chore: CORS 설정 enable

--- pocketpt/src/main/java/com/madeyepeople/pocketpt/global/config/SecurityConfig.java
+++ pocketpt/src/main/java/com/madeyepeople/pocketpt/global/config/SecurityConfig.java
@@ -6,12 +6,15 @@ import com.madeyepeople.pocketpt.domain.account.social.RedirectAuthenticationSuc
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
@@ -28,8 +31,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(withDefaults())
                 .csrf((csrf) -> csrf.disable())
-//                .cors((cors) -> cors
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/main").permitAll()
