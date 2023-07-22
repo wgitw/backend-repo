d4531204d38690d96b43c068054fdd0d5da08ef8
san <55998816+kimtks456@users.noreply.github.com>
2023-07-23 00:35:13 +0900

    chore: CORS 수정

--- pocketpt/src/main/java/com/madeyepeople/pocketpt/global/config/CorsConfig.java
+++ pocketpt/src/main/java/com/madeyepeople/pocketpt/global/config/CorsConfig.java
@@ -19,8 +19,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-//        config.setAllowedOrigins(List.of("http://localhost:8000", corsFrontend));
-        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:8000", corsFrontend));
+//        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
