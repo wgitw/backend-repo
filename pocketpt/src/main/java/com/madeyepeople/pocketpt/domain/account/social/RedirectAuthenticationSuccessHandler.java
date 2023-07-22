ef6e6c4cbe98849ac590101072acd3bf33e5c141
san <55998816+kimtks456@users.noreply.github.com>
2023-07-23 02:06:35 +0900

    test: cookie

--- pocketpt/src/main/java/com/madeyepeople/pocketpt/domain/account/social/RedirectAuthenticationSuccessHandler.java
+++ pocketpt/src/main/java/com/madeyepeople/pocketpt/domain/account/social/RedirectAuthenticationSuccessHandler.java
@@ -107,7 +107,7 @@ public class RedirectAuthenticationSuccessHandler extends SimpleUrlAuthenticatio
         response.addCookie(cookie_refresh);
 
         Cookie cookie_access = new Cookie(jwtUtil.COOKIE_KEY_ACCESS_TOKEN, accessToken);
-//        cookie_access.setDomain("localhost");
+        cookie_access.setDomain("http://43.201.57.199:8000");
         cookie_access.setPath("/");
         cookie_access.setSecure(true);
         cookie_access.setAttribute("SameSite", "None");
